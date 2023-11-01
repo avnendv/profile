4ed49b01cc3078860dd6af993a4a96e67f54e818
a2xvodoi <vietnguyen2x@gmail.com>
2023-11-01 21:38:20 +0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Structure with Gulp + HTML + SCSS + Bootstrap 5
---
+++ app/assets/js/pages/index.js
@@ -0,0 +1,12 @@
+/**
+ * Template Name:
+ * Updated:
+ * Template URL:
+ * Author: AVNENDV
+ * License:
+ */
+(function () {
+  "use strict";
+
+  console.warn("Welcome to console");
+})();
