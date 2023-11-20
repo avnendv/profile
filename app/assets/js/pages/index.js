8adc1366d7c1dd4740216cf43f7ee6443d0348e7
avnendv <viet.nd.avnendv@gmail.com>
2023-11-20 10:02:01 +0700

    feature: add profile

--- app/assets/js/pages/index.js
+++ app/assets/js/pages/index.js
@@ -8,5 +8,13 @@
 (function () {
   "use strict";
 
-  console.warn("Welcome to console");
+  console.warn("Welcome to console AVNENDV");
+
+  const typed = new Typed(".typed__text", {
+    strings: ["Frontend Develop", "Backend Develop"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
 })();
